dce6485c4019b3deaf88b258e123fd067fe1044d
nehal <nehal@nehal-Precision-T1500.(none)>
2013-02-20 09:59:14 -0800

    kdd changes added to xen

---
+++ kdd/.kdd-xen.o.d
@@ -0,0 +1,4 @@
+kdd-xen.o: kdd-xen.c \
+ /shared/home/nehalb/build_ws_2.hg/myrepos/xen-4.1.hg/tools/debugger/kdd/../../../tools/libxc/xenctrl.h \
+ /shared/home/nehalb/build_ws_2.hg/myrepos/xen-4.1.hg/tools/debugger/kdd/../../../tools/libxc/xentoollog.h \
+ kdd.h
